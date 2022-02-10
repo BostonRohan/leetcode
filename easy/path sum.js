6efe9af50c746aea697372812dd6fd0a019a9ae8
Boston Rohan <bostonrohan@gmail.com>
2022-02-10 15:39:20 -0800

    formatted path sum

--- easy/path sum.js
+++ easy/path sum.js
@@ -1,7 +1,10 @@
 var hasPathSum = function (root, targetSum) {
   if (!root) return false;
 
-  if (root - targetSum === 0 && !root.left && !root.right) return true;
+  if (targetSum === root.val && !root.left && !root.right) return true;
 
-  return hasPathSum(root, targetSum);
+  return (
+    hasPathSum(root.left, targetSum - root.val) ||
+    hasPathSum(root.right, targetSum - root.val)
+  );
 };
