ee2fab6faef6e1957b2762fca7c66c145709aa9d
Boston Rohan <bostonrohan@gmail.com>
2022-02-09 17:56:48 -0800

    completed minimum depth of a binary tree

---
+++ easy/minimum depth of a binary tree.js
@@ -0,0 +1,36 @@
+var minDepth = function (root) {
+  if (!root) return 0;
+
+  let queue = [];
+  queue.push(root);
+  let depth = 0;
+
+  while (queue.length > 0) {
+    let nodes = queue.length;
+
+    while (nodes > 0) {
+      let curr = queue.shift();
+
+      if (curr.left) queue.push(curr.left);
+
+      if (curr.right) queue.push(curr.right);
+
+      if (!curr.left && !curr.right) return depth + 1;
+
+      nodes--;
+    }
+    depth++;
+  }
+};
+
+//queue implementation
+
+//(first one to be removed, is the first one added)
+
+//[1] [1, 2] [1, 2, 3]
+
+//[2, 3]
+
+//[3]
+
+//breath first search
