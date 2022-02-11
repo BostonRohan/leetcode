d54730288e795ede48091cf8ac8ebf1e81f85aa3
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 09:42:02 -0800

    timed valid parenthesis

--- easy/valid parentheses.js
+++ easy/valid parentheses.js
@@ -1,17 +1,35 @@
-var isValid = function (s) {
+// var isValid = function (s) {
+//   let brackets = { "(": ")", "{": "}", "[": "]" };
+//   let arr = [];
+//   for (let char of s) {
+//     //Finds the key (opening bracket), and checks for the corresponding closing bracket
+//     if (brackets[char]) {
+//       //Pushes the opening bracket
+//       arr.push(brackets[char]);
+//     } else {
+//       //Checks the last element of the arr to see if it corresponds to the closing bracket
+//       if (arr.pop() !== char) return false;
+//     }
+//   }
+//   //Returns true since the array should be zero
+//   return !arr.length;
+// };
+// //https://www.youtube.com/watch?v=wtynhUwS5hI
+
+//Second Solution (10 min timer)
+const isValid = (s) => {
   let brackets = { "(": ")", "{": "}", "[": "]" };
-  let arr = [];
-  for (let char of s) {
-    //Finds the key (opening bracket), and checks for the corresponding closing bracket
-    if (brackets[char]) {
-      //Pushes the opening bracket
-      arr.push(brackets[char]);
-    } else {
-      //Checks the last element of the arr to see if it corresponds to the closing bracket
-      if (arr.pop() !== char) return false;
+  const open = Object.keys(brackets);
+  const closed = Object.values(brackets);
+
+  for (let i = 0; i < s.length; i++) {
+    if (
+      open.indexOf(
+        brackets[s[i]] > 0 && closed.indexOf(brackets[s[i - s.length]])
+      ) > 0
+    ) {
+      return true;
     }
   }
-  //Returns true since the array should be zero
-  return !arr.length;
+  return false;
 };
-//https://www.youtube.com/watch?v=wtynhUwS5hI
