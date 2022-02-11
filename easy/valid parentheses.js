c432064662210c0030b2aafcb169e05bc54659ef
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 09:42:35 -0800

    uncommented correct solution

--- easy/valid parentheses.js
+++ easy/valid parentheses.js
@@ -1,20 +1,20 @@
-// var isValid = function (s) {
-//   let brackets = { "(": ")", "{": "}", "[": "]" };
-//   let arr = [];
-//   for (let char of s) {
-//     //Finds the key (opening bracket), and checks for the corresponding closing bracket
-//     if (brackets[char]) {
-//       //Pushes the opening bracket
-//       arr.push(brackets[char]);
-//     } else {
-//       //Checks the last element of the arr to see if it corresponds to the closing bracket
-//       if (arr.pop() !== char) return false;
-//     }
-//   }
-//   //Returns true since the array should be zero
-//   return !arr.length;
-// };
-// //https://www.youtube.com/watch?v=wtynhUwS5hI
+var isValid = function (s) {
+  let brackets = { "(": ")", "{": "}", "[": "]" };
+  let arr = [];
+  for (let char of s) {
+    //Finds the key (opening bracket), and checks for the corresponding closing bracket
+    if (brackets[char]) {
+      //Pushes the opening bracket
+      arr.push(brackets[char]);
+    } else {
+      //Checks the last element of the arr to see if it corresponds to the closing bracket
+      if (arr.pop() !== char) return false;
+    }
+  }
+  //Returns true since the array should be zero
+  return !arr.length;
+};
+//https://www.youtube.com/watch?v=wtynhUwS5hI
 
 //Second Solution (10 min timer)
 const isValid = (s) => {
