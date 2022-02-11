5a4067489d986d6790f4d36e2f6838d182de7028
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 08:50:50 -0800

    added correct timed solution

--- easy/palindrome number.js
+++ easy/palindrome number.js
@@ -4,3 +4,15 @@ var isPalindrome = function (x) {
   if (x === backward) return true;
   else return false;
 };
+
+//Second Solution (10 min timer)
+
+const isPalindrome = (x) => {
+  let arr = x.toString().split("");
+  let sum = "";
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    sum += arr[i];
+  }
+  return +sum === x;
+};
