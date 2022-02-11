357d12ef006aba8c6e97fb443db917fd3efcf33a
Boston Rohan <bostonrohan@gmail.com>
2022-02-11 09:27:05 -0800

    uncommented original solution

--- easy/longest common prefix.js
+++ easy/longest common prefix.js
@@ -1,22 +1,22 @@
-// var longestCommonPrefix = (strs) => {
-//   if (strs.length === 1) return strs[0];
+var longestCommonPrefix = (strs) => {
+  if (strs.length === 1) return strs[0];
 
-//   let res = strs[0];
-//   let i = 1;
+  let res = strs[0];
+  let i = 1;
 
-//   //While res exists and i is less than the string length
-//   while (res && i < strs.length) {
-//     //If the string starts with res
-//     if (strs[i].startsWith(res)) {
-//       i++;
-//     } else {
-//       //Cuts off the end of the word, until the longest prefix remains
-//       res = res.slice(0, res.length - 1);
-//     }
-//   }
+  //While res exists and i is less than the string length
+  while (res && i < strs.length) {
+    //If the string starts with res
+    if (strs[i].startsWith(res)) {
+      i++;
+    } else {
+      //Cuts off the end of the word, until the longest prefix remains
+      res = res.slice(0, res.length - 1);
+    }
+  }
 
-//   return res;
-// };
+  return res;
+};
 
 //Second Solution (10 min timer)
 
