8b4ee5ba3fedd7a091420b48c1ed88822781e611
Boston Rohan <bostonrohan@gmail.com>
2022-03-01 19:12:06 -0800

    unfinished return

---
+++ easy/return/excel sheet column title.js
@@ -0,0 +1,6 @@
+var convertToTitle = function (columnNumber) {
+  // if (columnNumber <= 26) return String.fromCharCode(64 + columnNumber);
+  return String.fromCharCode(64 + 1);
+};
+console.log(convertToTitle(28));
+//1 - 26 are in their regular positions (a-z)
