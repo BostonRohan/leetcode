c4ff04d471a0e316343b704fdfb234f079626b4b
Boston Rohan <bostonrohan@gmail.com>
2022-03-08 11:38:07 -0800

    completed

---
+++ easy/tree/ts/binary tree preorder traversal.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from "./type";
+function preorderTraversal(root: TreeNode | null): number[] {
+  let ans: number[] = [];
+  let stack: TreeNode[] = [root];
+  if (!root) return ans;
+
+  while (stack.length > 0) {
+    const node: TreeNode = stack.pop();
+
+    if (!node) continue;
+
+    ans.push(node.val);
+    stack.push(node.right);
+    stack.push(node.left);
+  }
+  return ans;
+}
+
+//Recursive solution??
